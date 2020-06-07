807eb468b1a320ee88574b70d988ce550fb35b45
tw97223 <tw97223@gmail.com>
2020-06-07 15:35:48 +0900

    changed to "this is ver-1.0"

--- TestRepository/SecondViewController.swift
+++ TestRepository/SecondViewController.swift
@@ -16,7 +16,7 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let label = UILabel()
-        label.text = "SHIRODAMA"
+        label.text = "This is ver-1.0"
         view.addSubview(label)
     }
     
